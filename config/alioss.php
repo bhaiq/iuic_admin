cb44576c83dd6bde3417b4a75d3534885b9d9ed1
bhai <2283814227@qq.com>
2020-09-03 17:13:55 +0800

    更换阿里云云存储

--- config/alioss.php
+++ config/alioss.php
@@ -4,7 +4,7 @@ return [
     'useInternal' => false,//是否使用OSS内网传输来省流量
     'ossServer' => 'oss-cn-hongkong.aliyuncs.com',
     'ossServerInternal' => 'http://oss-cn-hongkong-internal.aliyuncs.com',
-    'AccessKeyId' => 'LTAIg78qm6EvjaAq',
-    'AccessKeySecret' => 'bbvhgInXYToGy0AQ4X24R9aATUAOW3',
-    'bucket' => 'iuic',
+    'AccessKeyId' => 'LTAI4G5FEcu9wwa36YpHGdaY',
+    'AccessKeySecret' => 'KvNXRkff0Wcz6i3DlmXqADQD4rpHDX',
+    'bucket' => 'iuics',
 ];
