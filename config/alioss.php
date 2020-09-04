ae32714ac9a46eadbd9b8dd9c79cd8675f34bf09
bhai <40416030+bhaiq@users.noreply.github.com>
2020-09-04 09:28:09 +0800

    Update alioss.php

--- config/alioss.php
+++ config/alioss.php
@@ -4,7 +4,7 @@ return [
     'useInternal' => false,//是否使用OSS内网传输来省流量
     'ossServer' => 'oss-cn-hongkong.aliyuncs.com',
     'ossServerInternal' => 'http://oss-cn-hongkong-internal.aliyuncs.com',
-    'AccessKeyId' => 'LTAI4G5FEcu9wwa36YpHGdaY',
-    'AccessKeySecret' => 'KvNXRkff0Wcz6i3DlmXqADQD4rpHDX',
+    'AccessKeyId' => '',
+    'AccessKeySecret' => '',
     'bucket' => 'iuics',
 ];
