ac42e598370a73c2677b80363192b68c6e8c218a
bhai <2283814227@qq.com>
2020-12-03 16:59:33 +0800

    增加月度分红记录

--- app/Http/Controllers/Admin/BonusRecordController.php
+++ app/Http/Controllers/Admin/BonusRecordController.php
@@ -17,58 +17,34 @@ class BonusRecordController extends Controller
             $page = $request->get('page', 1);
             $limit = $request->get('limit', 10);
             $soso = $request->get('soso', 0);
-//            $start = ($page - 1) * $limit;
-//            $p = AccountLog::from('account_log as al')
-//                ->select('al.*', 'u.new_account as mobile', 'c.name as coin_name', 'a.name as realname')
-//                ->join('user as u', 'u.id', 'al.uid')
-//                ->join('coin as c', 'c.id', 'al.coin_id')
-//                ->leftJoin('authentication as a', 'a.uid', 'al.uid')
-//                ->where('al.remark','like','%'.'分红'.'%')
-//
-            $p = AccountLog::with('user')
-                ->with('coin')
-                ->where('remark','like','%'.'分红'.'%');
 
-//            $sql = "select al.*,u.new_account as mobile, c.name as coin_name, a.name as realname
-//                    from account_log as al
-//                    left join user as u on u.id = al.uid
-//                    left join coin as c on c.id = al.coin_id
-//                    left join authentication as a on a.uid = al.uid
-//                    where al.remark like '分红%'";
-//
-//            $countSql = "select 1
-//                    from account_log as al
-//                    left join user as u on u.id = al.uid
-//                    left join coin as c on c.id = al.coin_id
-//                    left join authentication as a on a.uid = al.uid
-//                    where al.remark like '分红%' and al.id >  $start";
+            $p = AccountLog::from('account_log as al')
+                ->select('al.uid','al.coin_id','al.remark', 'u.new_account as mobile', 'c.name as coin_name', 'a.name as realname')
+                ->join('user as u', 'u.id', 'al.uid')
+                ->join('coin as c', 'c.id', 'al.coin_id')
+                ->leftJoin('authentication as a', 'a.uid', 'al.uid')
+                ->where('al.remark','like','%','分红','%');
 
             // 筛选条件
-//            if ($soso) {
-//                $p->where(function ($query) use ($soso) {
-//                    $query->where('u.new_account', 'like', '%' . $soso . '%')
-//                        ->orwhere('c.name', 'like', '%' . $soso . '%')
-//                        ->orwhere('a.name', 'like', '%' . $soso . '%');
-//                });
-////                $sql .= " and (u.new_account like  '$soso%' or c.name like '$soso%' or a.name like '$soso%')";
-////                $countSql .= " and (u.new_account like  '$soso%' or c.name like '$soso%' or a.name like '$soso%')";
-//            }
-//            dd($p);
+            if ($soso) {
+                $p->where(function ($query) use ($soso) {
+                    $query->where('u.new_account', 'like', '%' . $soso . '%')
+                        ->orwhere('c.name', 'like', '%' . $soso . '%')
+                        ->orwhere('a.name', 'like', '%' . $soso . '%')
+                        ->orwhere('al.remark', 'like', '%' . $soso . '%');
+                });
+            }
+
             $data['code'] = 0;
             $data['msg'] = '查询成功';
             $data['count'] = $p->count();
-//            $data['count'] = DB::query($countSql);
 
-//            $sql .= " order by al.created_at desc limit $limit";
-//            echo $sql;die;
-//            $p->orderBy('created_at','desc')->skip(($page - 1) * $limit);
-            $p->orderBy('created_at','desc')->paginate($limit);
+            $p->latest('al.created_at')->skip(($page - 1) * $limit)->take($limit);
             $data['data'] = $p->get()->toArray();
-//            $data['data'] = DB::query($sql);
 
             return response()->json($data);
         }
 
-        return view('admin.bonus_record.index');
+        return view('admin.wallet_log.index');
     }
 }
