155566e01da483e3f71d1bf01ff18da91047ca12
bhai <2283814227@qq.com>
2020-12-03 17:18:52 +0800

    增加月度分红记录

--- app/Http/Controllers/Admin/BonusRecordController.php
+++ app/Http/Controllers/Admin/BonusRecordController.php
@@ -19,7 +19,8 @@ class BonusRecordController extends Controller
             $soso = $request->get('soso', 0);
 
             $p = AccountLog::from('account_log as al')
-                ->select('al.uid','al.coin_id','al.remark', 'u.new_account as mobile', 'c.name as coin_name', 'a.name as realname')
+                ->select('al.uid','al.coin_id','al.remark', 'al.amount','al.created_at',
+                    'u.new_account as mobile', 'c.name as coin_name', 'a.name as realname')
                 ->leftjoin('user as u', 'u.id', 'al.uid')
                 ->leftjoin('coin as c', 'c.id', 'al.coin_id')
                 ->leftJoin('authentication as a', 'a.uid', 'al.uid')
