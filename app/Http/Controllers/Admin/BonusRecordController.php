25405b7b4b8c8e716e8f57fdeb3aa4940f2aa5f0
bhai <2283814227@qq.com>
2020-12-03 17:16:19 +0800

    增加月度分红记录

--- app/Http/Controllers/Admin/BonusRecordController.php
+++ app/Http/Controllers/Admin/BonusRecordController.php
@@ -22,7 +22,7 @@ class BonusRecordController extends Controller
                 ->select('al.uid','al.coin_id','al.remark', 'u.new_account as mobile', 'c.name as coin_name', 'a.name as realname')
                 ->leftjoin('user as u', 'u.id', 'al.uid')
                 ->leftjoin('coin as c', 'c.id', 'al.coin_id')
-//                ->leftJoin('authentication as a', 'a.uid', 'al.uid')
+                ->leftJoin('authentication as a', 'a.uid', 'al.uid')
                 ->where('al.remark','like','%'.'分红'.'%');
 
             // 筛选条件
