4eb41cd38fa86f4292860ee099cd49028e836c32
bhai <2283814227@qq.com>
2020-12-03 17:40:04 +0800

    增加月度分红记录

--- app/Http/Controllers/Admin/BonusRecordController.php
+++ app/Http/Controllers/Admin/BonusRecordController.php
@@ -28,20 +28,21 @@ class BonusRecordController extends Controller
                 ->where('al.created_at','>','2020-01-01');
 
             // 筛选条件
-            if ($soso) {
-                $p->where(function ($query) use ($soso) {
-                    $query->where('u.new_account', 'like', '%' . $soso . '%')
-                        ->orwhere('c.name', 'like', '%' . $soso . '%')
-                        ->orwhere('a.name', 'like', '%' . $soso . '%')
-                        ->orwhere('al.remark', 'like', '%' . $soso . '%');
-                });
-            }
+//            if ($soso) {
+//                $p->where(function ($query) use ($soso) {
+//                    $query->where('u.new_account', 'like', '%' . $soso . '%')
+//                        ->orwhere('c.name', 'like', '%' . $soso . '%')
+//                        ->orwhere('a.name', 'like', '%' . $soso . '%')
+//                        ->orwhere('al.remark', 'like', '%' . $soso . '%');
+//                });
+//            }
 
             $data['code'] = 0;
             $data['msg'] = '查询成功';
             $data['count'] = $p->count();
 
-            $p->latest('al.created_at')->skip(($page - 1) * $limit)->take($limit);
+//            $p->latest('al.created_at')->skip(($page - 1) * $limit)->take($limit);
+            $p->latest('al.created_at')->paginate($limit);
             $data['data'] = $p->get()->toArray();
 
             return response()->json($data);
