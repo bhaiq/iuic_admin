5837073938e5158e25c806563e16070d21c86a6d
bhai <2283814227@qq.com>
2020-12-03 18:49:45 +0800

    增加月度分红记录

--- app/Http/Controllers/Admin/BonusRecordController.php
+++ app/Http/Controllers/Admin/BonusRecordController.php
@@ -49,6 +49,11 @@ class BonusRecordController extends Controller
 //            $p->latest('al.created_at')->paginate($limit);
             foreach ($p as $k => $v){
                 $data['data'][$k]['mobile'] = $v->user->mobile;
+                $data['data'][$k]['coin_name'] = $v->coin->coin_name;
+                $data['data'][$k]['amount'] = $v->amount;
+                $data['data'][$k]['type'] = $v->type;
+                $data['data'][$k]['remark'] = $v->remark;
+                $data['data'][$k]['created_at'] = $v->created_at;
             }
 
 
