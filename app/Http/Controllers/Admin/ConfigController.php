a05d5704cfe42b12f3d87de4b4e5d7cfdfe9f37c
bhai <2283814227@qq.com>
2021-01-06 15:31:31 +0800

    测试

--- app/Http/Controllers/Admin/ConfigController.php
+++ app/Http/Controllers/Admin/ConfigController.php
@@ -233,7 +233,7 @@ class ConfigController extends Controller
 
         $arr6 = config('reward');
         $arr10 = config('admin_mall');
-
+        return 1;
         \Log::info('修改前数据', [$arr1, $arr2, $arr3, $arr4, $arr5, $arr6, $arr7, $arr8, $arr9, $arr10, $arr11, $arr12, $arr13]);
 
         if(is_array($arr1)){
