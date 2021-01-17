4fcdf3a4cad30b3ad86d8c3e84337b7d920a4a8d
bhai <2283814227@qq.com>
2021-01-17 17:39:18 +0800

    团队长加速分红奖后台编辑

--- app/Http/Controllers/Admin/SpeedBonusController.php
+++ app/Http/Controllers/Admin/SpeedBonusController.php
@@ -63,14 +63,13 @@ class SpeedBonusController extends Controller
     // 添加新增团队长分红用户
     public function store(Request $request)
     {
-        dd($this->fields);
+
         $ver = new SpeedBounus();
         foreach (array_keys($this->fields) as $field) {
             $ver->$field = $request->get($field);
         }
 
         $ver->save();
-        return 1;
         AdminLog::addLog('新增一个团队长加速分红用户');
 
         return redirect('/admin/speed_bonus/index');
