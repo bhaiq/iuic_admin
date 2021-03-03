e14e943b6ce6ca58f2c8f7541f57a3728068029c
bhai <2283814227@qq.com>
2021-03-03 16:20:03 +0800

    手续费日结算表

--- app/Models/EcologyServiceDay.php
+++ app/Models/EcologyServiceDay.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class EcologyServiceDay extends Model
 {
     //
+    protected $table = 'ecology_service_day';
+
+    protected $guarded = [];
+
+    //类型
+    public function getSetStatusAttribute($value) {
+        $arr = ['0'=>'未结算','1'=>'自动','2'=>'手动'];
+        return ['msg'=>$arr[$value],'value'=>$value];
+    }
 }
