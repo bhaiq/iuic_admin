c2c7f6222a16bdae57cdc02768c09bb4860db8a4
bhai <2283814227@qq.com>
2021-03-16 14:47:46 +0800

    后台更改积分钱包模块

--- app/Http/Controllers/Admin/EcologyCarrankingController.php
+++ app/Http/Controllers/Admin/EcologyCarrankingController.php
@@ -35,7 +35,8 @@ class EcologyCarrankingController extends Controller
 
             $p->skip(($page - 1) * $limit)->take($limit);
             $data['data'] = $p->get()->toArray();
-            $level = array_flip(EcologyConfig::all()->toArray());
+            $level = EcologyConfig::all()->toArray();
+            $level = array_flip($level);
             foreach ($data['data'] as $k => $v){
                 $data['data'][$k]['carranking'] = $k+(($page-1)*$limit)+1;
                 $data['data'][$k]['level_name'] = array_keys(array_column($level,'name'),$v['ecology_lv']);
