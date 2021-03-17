ffbeb49ea9974ac4148b2c73f221d99ee8d2ec51
bhai <2283814227@qq.com>
2021-03-17 14:56:52 +0800

    后台更改积分钱包模块

--- app/Http/Controllers/Admin/ConfigController.php
+++ app/Http/Controllers/Admin/ConfigController.php
@@ -430,21 +430,7 @@ class ConfigController extends Controller
 
         }
 
-        if(is_array($arr14)){
-            foreach ($arr14 as $k => $v){
-
-                if($request->has($k)){
-                    $arr14[$k] = $request->get($k);
-                }
-
-            }
-
-            $text = "<?php return ".var_export($arr14,true).";";
-            file_put_contents($url14, $text);
-
-        }
-
-        \Log::info('修改后数据', [$arr1, $arr2, $arr3, $arr4, $arr5, $arr6, $arr7, $arr8, $arr9, $arr10, $arr11, $arr12, $arr13, $arr14]);
+        \Log::info('修改后数据', [$arr1, $arr2, $arr3, $arr4, $arr5, $arr6, $arr7, $arr8, $arr9, $arr10, $arr11, $arr12, $arr13]);
 
         AdminLog::addLog('修改了配置');
 
