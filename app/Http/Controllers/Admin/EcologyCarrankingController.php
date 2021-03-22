49bb46694378ea47ab16492a4046da16c842fc9f
bhai <2283814227@qq.com>
2021-03-22 13:57:57 +0800

    后台更改积分钱包模块

--- app/Http/Controllers/Admin/EcologyCarrankingController.php
+++ app/Http/Controllers/Admin/EcologyCarrankingController.php
@@ -25,7 +25,7 @@ class EcologyCarrankingController extends Controller
                 ->when($soso, function ($query) use ($soso) {
                     return $query->where('u.new_account', 'like', "%{$soso}%");
                 })
-                ->leftJoin('authentication as aa', 'aa.uid', 'u.uid')
+                ->leftJoin('authentication as aa', 'aa.uid', 'u.id')
                 ->orderBy('u.ecology_lv','desc')
                 ->orderBy('u.ecology_lv_time','asc')
                 ->orderBy('u.car_is_show','desc');
